dc39e0d9c155ccefe9ab4688500419121a6801fc
Rob Taussig <robert.taussig@gmail.com>
2019-07-31 19:43:02 -0400

    First commit

---
+++ src/App/__tests__/index.test.js
@@ -0,0 +1,5 @@
+describe('App', () => {
+  test('It works', () => {
+    expect(true).toBe(true);
+  });
+});
---
+++ src/App/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+export const App = ({}) => {
+  return (
+    <div>
+      Hello from App
+    </div>
+  );
+};
+
+export default App;
